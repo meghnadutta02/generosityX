7c72c8c09c29ef51d19992960f1ca77653e093f8
Ankush Roy <imankushroy@gmail.com>
2023-07-12 20:49:35 +0530

    New chatbot service added

--- frontend/src/components/Chatbot.jsx
+++ frontend/src/components/Chatbot.jsx
@@ -8,25 +8,12 @@ function Chatbot() {
   };
   return (
     <div>
-      <button
-        className="fixed bottom-8 bg-blue-700 rounded-full border-black border-2 right-6 p-3 z-10"
-        onClick={togglePopup}
-      >
-        <InsertCommentRoundedIcon
-          fontSize="large"
-          sx={{ color: "orange" }}
-        ></InsertCommentRoundedIcon>
-      </button>
-      {isOpen && (
-        <div className="popup fixed bottom-16 right-2 p-4 m-2 z-10">
-          <iframe
-            width="350"
-            height="430"
-            allow="microphone;"
-            src="https://console.dialogflow.com/api-client/demo/embedded/713f38ad-c406-441a-b49e-6f36dd5f1bd6"
-          ></iframe>
-        </div>
-      )}
+      <df-messenger
+        intent="WELCOME"
+        chat-title="Helpy"
+        agent-id="af682445-7d3d-40ea-8c84-a479f26fc35b"
+        language-code="en"
+      ></df-messenger>
     </div>
   );
 }
