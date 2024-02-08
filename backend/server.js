d5a42d736d9639aff3b11bd555101f2c0eefed1a
Ankush Roy <imankushroy@gmail.com>
2024-02-09 00:41:29 +0530

    Enabled cors to allow deployed frontend traffic

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,31 @@
 const express = require("express");
-const fileUpload=require("express-fileupload")
-const cookieParser=require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.json()); // for parsing application/json
 //no need of installing bodyparser separately
-app.use(fileUpload());//to upload files
+app.use(fileUpload()); //to upload files
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://generosityx.onrender.com"],
+  })
+);
 const apiRoutes = require("./routes/apiRoutes");
-const {rsvped}=require("./controllers/campaignControllers");
-app.post("/rsvp",rsvped)
-app.get("/", async (req, res,next) => {
-  
- res.json({ message: "API running" });
-})
+const { rsvped } = require("./controllers/campaignControllers");
+app.post("/rsvp", rsvped);
+app.get("/", async (req, res, next) => {
+  res.json({ message: "API running" });
+});
 //mongodb connection
 const connect = require("./config/db.js");
 connect();
 app.use("/api", apiRoutes);
-app.use((err,req,res,next)=>{
-    res.status(500).json(
-        {
-            message:err.message,
-            stack:err.stack
-        }
-    )
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
 });
 app.listen(5000, () => console.log("Server started at port 5000"));
-
